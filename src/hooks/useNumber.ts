8d984a32353c4a1f9efb673be5bec5c0432ff6ab
Dmitri Levchenko <dmitrilevchenko23@gmail.com>
2024-03-27 12:40:04 +0200

    REMOVE | unused code

--- src/hooks/useNumber.ts
+++
@@ -1,9 +0,0 @@
-import React, {useDebugValue, useState} from 'react'
-
-export const useNumber = () => {
-  const [state, setState] = useState<number>()
-
-  useDebugValue(state ? 'Has Value' : 'No Value')
-
-  return {state, setState}
-}
